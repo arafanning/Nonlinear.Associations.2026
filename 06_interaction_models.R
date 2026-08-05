56a1e60d80d22c8abe0d0f8fdf1d02600836bca2
Ashley Adams-Fanning <146988745+arafanning@users.noreply.github.com>
2026-08-04 20:48:16 -0400

    Delete 06_interaction_models.R

--- 06_interaction_models.R
+++
@@ -1,7 +0,0 @@
-# Nonlinear Associations Between Cannabis Use and Neurocognition Among People Living With HIV
-# Script: 06_interaction_models.R
-# Purpose: 06 Interaction Models
-#
-# Remove participant identifiers, local usernames, credentials,
-# restricted output, and machine-specific file paths before publication.
-
